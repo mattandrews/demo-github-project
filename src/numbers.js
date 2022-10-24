1e1fdec3a485d89f4ae77692ebfed090fb33e4ad
Matt Andrews <matthewandrews@economist.com>
2022-10-24 08:48:42 +0100

    Add some really great new functions

--- src/numbers.js
+++ src/numbers.js
@@ -10,8 +10,33 @@ const multiplyByTen = x => {
   return multiplyNumbers(x, 10);
 }
 
+const addTen = x => {
+  let total = x;
+  total = total + 1;
+  total = total + 1;
+  total = total + 1;
+  total = total + 1;
+  total = total + 1;
+  total = total + 1;
+  total = total + 1;
+  total = total + 1;
+  total = total + 1;
+  total = total + 1;
+  return total;
+}
+
+const addTenV2 = x => {
+  let total = x;
+  for (let i = 0; i < 10; i++) {
+    total += 1;
+  }
+  return total;
+}
+
 module.exports = {
   addNumbers,
   multiplyNumbers,
-  multiplyByTen
+  multiplyByTen,
+  addTen,
+  addTenV2
 }
